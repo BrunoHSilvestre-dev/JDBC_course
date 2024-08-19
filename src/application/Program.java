e8bab8de8c9a96393aa14b9504951af63caac9b6
BrunoHSilvestre-dev <brunosilvestre201010@gmail.com>
2024-08-19 17:31:31 -0300

    Select example

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,42 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import db.DB;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Connection conn = DB.getConnection();	
-		DB.closeConnection();
+//		Connection conn = DB.getConnection();	
+//		DB.closeConnection();
+		
+		selectExample();
 	}
 
+	private static void selectExample() {
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			
+			rs = st.executeQuery("select * from department");
+			
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + " " + rs.getString("Name"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+	
 }
